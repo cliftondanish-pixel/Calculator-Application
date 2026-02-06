35a92fb44716dcca22a9270bdc47d84dc47f64b0
CliftonDanishJ <cliftondanish@thestackly.com>
2026-02-06 15:00:11 +0530

    Add initial HTML, CSS, and JavaScript files for calculator application

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.querySelector("input");
+
+function calc(x) {
+  input.value = input.value + x;
+}
+function oper() {
+  input.value = parseFloat(input.value) * -1;
+}
+function byx() {
+  input.value = 1 / parseFloat(input.value);
+}
+function pow() {
+  input.value = Math.pow(parseFloat(input.value), 2);
+}
+function sqr() {
+  input.value = Math.sqrt(parseFloat(input.value));
+}
+function clr() {
+  input.value = "";
+}
+function del() {
+  input.value = input.value.slice(0, -1);
+}
+function equal() {
+  input.value = eval(input.value);
+}
