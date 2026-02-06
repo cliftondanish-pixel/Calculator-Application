cc39b34892138b0566240d5c090bd3c9c53130ae
CliftonDanishJ <cliftondanish@thestackly.com>
2026-02-06 15:33:42 +0530

    Refactor mathematical operations to remove unnecessary parseFloat calls

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ function calc(x) {
   input.value = input.value + x;
 }
 function oper() {
-  input.value = parseFloat(input.value) * -1;
+  input.value = (input.value) * -1;
 }
 function byx() {
-  input.value = 1 / parseFloat(input.value);
+  input.value = 1 / (input.value);
 }
 function pow() {
-  input.value = Math.pow(parseFloat(input.value), 2);
+  input.value = Math.pow((input.value), 2);
 }
 function sqr() {
-  input.value = Math.sqrt(parseFloat(input.value));
+  input.value = Math.sqrt(input.value);
 }
 function clr() {
   input.value = "";
